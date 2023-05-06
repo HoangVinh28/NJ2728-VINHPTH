771f6eff11366eabded30b00951b790ad5d3caef
HoangVinh28 <phanthoangvinh28@gmail.com>
2023-05-06 14:05:06 +0700

    update code

---
+++ Nextjs/my-app/src/libraries/axiosClient.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { TOKEN, REFRESH_TOKEN } from '../constants/index';
+
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {'Content-Type': 'application/json'},
+});
+
+export default axiosClient;
